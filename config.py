5a93462da5fd12e4f3acf0bb7747be705452e231
vovanchubenko <112155197+vovanchubenko@users.noreply.github.com>
2022-08-26 14:09:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+API_TOKEN = '5796227133:AAGjNB_wi6chVwa5HMLEGz48YXaEDIQHWLI' # токен fatherbot
+number = '+' # номер киви кошелька
+QIWI_SEC_TOKEN = 'XXXXXXXXXXXXXXXXXX' # секретный ключ p2p https://p2p.qiwi.com
+sum = 99 # сумма которую пользователь должен оплатить
+admin = '5336123108' # id одмена
